d1614280b4972f3b3370bbde7288dcaa2d7423d7
Kathy Tong <100739687+kt-x@users.noreply.github.com>
2023-04-04 22:00:53 +0800

    input carpark.ppp.km rds file to app.r

--- app/app.R
+++ app/app.R
@@ -38,7 +38,7 @@ mpsz_sf <- read_rds("rds/mpsz_sf.rds")
 
 # HDB flats
 carpark_sf <- read_rds("rds/carpark_sf.rds")
-
+carpark_ppp_km <- read_rds("rds/carpark_ppp.km.rds")
 
 # hawker
 hawker_sf <- read_rds("rds/hawker_sf.rds")
